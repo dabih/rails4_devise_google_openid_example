6eb7f473d5f1ffadde966bc1a625978fde3dda88
Hiroaki Nakamura <hnakamur@gmail.com>
2013-11-29 18:59:31 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails4DeviseGoogleOpenidExample::Application.config.secret_key_base = '0e66274c92e68b2918b7632c880ab4f2cf15f06bb56b9ab9ea3acf3af7d76da67683a5fcb188d75b4fc864da4a227e55dd0b5e29db582a9bfdb7e845ed451016'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rails4DeviseGoogleOpenidExample::Application.config.session_store :cookie_store, key: '_rails4_devise_google_openid_example_session'
